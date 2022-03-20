ef062d4310c4a3ab5d5f29fb74ebedbdc98b18e0
atarek017 <atarek017@gmail.com>
2022-03-20 15:48:13 +0200

    edit check box "accept terms and conditions"

--- lib/logic/controllers/auth_controller.dart
+++ lib/logic/controllers/auth_controller.dart
@@ -3,9 +3,15 @@ import 'package:get/get.dart';
 
 class AuthController extends GetxController{
   bool isVisible=false;
+  bool isCheck=false;
 
   void visibility(){
     isVisible=!isVisible;
+    update();
+  }
+
+  void checkbox(){
+    isCheck=!isCheck;
     update();
   }
 }
--- lib/view/widgets/auth/check_widget.dart
+++ lib/view/widgets/auth/check_widget.dart
@@ -1,50 +1,58 @@
+import 'package:e_commerce_app_mvc/logic/controllers/auth_controller.dart';
 import 'package:e_commerce_app_mvc/view/widgets/text_utils.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:get/get_navigation/src/extension_navigation.dart';
+import 'package:get/get.dart';
 
 class CheckWidget extends StatelessWidget {
-  const CheckWidget({Key? key}) : super(key: key);
+  CheckWidget({Key? key}) : super(key: key);
+
+  final controller = Get.find<AuthController>();
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        InkWell(
-          onTap: () {},
-          child: Container(
-            height: 35,
-            width: 35,
-            decoration: BoxDecoration(
-                color: Colors.grey.shade200,
-                borderRadius: BorderRadius.circular(10)),
-            child:Get.isDarkMode? Image.asset("assets/images/check.png"):Icon(Icons.done,color: Colors.pink,size: 30,),
-          ),
-        ),
-        const SizedBox(
-          width: 10,
-        ),
-        Row(
-          children:  [
-            TextUtils(
-              text: "I accept ",
-              fontSize: 16,
-              fontWeight: FontWeight.normal,
-              color: Get.isDarkMode? Colors.black: Colors.white,
-              underLine: TextDecoration.none,
+    return GetBuilder<AuthController>(builder: (_) {
+      return Row(
+        children: [
+          InkWell(
+            onTap: () {
+              controller.checkbox();
+            },
+            child: Container(
+              height: 35,
+              width: 35,
+              decoration: BoxDecoration(
+                  color: Colors.grey.shade200,
+                  borderRadius: BorderRadius.circular(10)),
+              child:controller.isCheck? Icon(
+                Icons.done,
+                color: Get.isDarkMode ? Colors.green : Colors.pink,
+                size: 30,
+              ):SizedBox(),
             ),
-
-             TextUtils(
-              text: "terms & conditions",
-              fontSize: 16,
-              fontWeight: FontWeight.normal,
-               color: Get.isDarkMode? Colors.black: Colors.white,
-              underLine: TextDecoration.underline,
-            )
-
-          ],
-        )
-      ],
-    );
+          ),
+          const SizedBox(
+            width: 10,
+          ),
+          Row(
+            children: [
+              TextUtils(
+                text: "I accept ",
+                fontSize: 16,
+                fontWeight: FontWeight.normal,
+                color: Get.isDarkMode ? Colors.black : Colors.white,
+                underLine: TextDecoration.none,
+              ),
+              TextUtils(
+                text: "terms & conditions",
+                fontSize: 16,
+                fontWeight: FontWeight.normal,
+                color: Get.isDarkMode ? Colors.black : Colors.white,
+                underLine: TextDecoration.underline,
+              )
+            ],
+          )
+        ],
+      );
+    });
   }
 }
